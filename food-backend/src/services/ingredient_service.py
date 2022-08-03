f83d815fd24c2ef77a3e7bf82047e3afce7b99b9
Louise <lowa1661@gmail.com>
2022-08-03 22:13:16 +0200

    Add ingredient endpoint

--- food-backend/src/services/ingredient_service.py
+++ food-backend/src/services/ingredient_service.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, jsonify
+from sqlalchemy.exc import IntegrityError
+from flask import Blueprint, jsonify, request, make_response
 
 from src.entities.entity import Session
 from src.entities.ingredient import Ingredient, IngredientSchema
@@ -19,3 +20,30 @@ def get_ingredients():
     session.close()
     # serializing as JSON
     return jsonify(ingredients), 200
+
+
+@ingredient_blueprint.route("/ingredients", methods=["POST"])
+def add_ingredient():
+    try:
+        posted_ingredient = IngredientSchema(only=("name", "rating")).load(
+            request.get_json()
+        )
+
+        ingredient = Ingredient(**posted_ingredient)
+        if ingredient.rating > 10 or ingredient.rating < 0:
+            raise ValueError
+
+        session = Session()
+        session.add(ingredient)
+        session.commit()
+
+        new_ingredient = IngredientSchema().dump(ingredient)
+        session.close()
+        return jsonify(new_ingredient), 201
+    except ValueError:
+        error_message = "Rating must fall in range [0, 10]."
+    except IntegrityError:
+        error_message = "Ingredient name already exists."
+    except Exception:
+        error_message = "Something went wrong."
+    return make_response(error_message, 400)
