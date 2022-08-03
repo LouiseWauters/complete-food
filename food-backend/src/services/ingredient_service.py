fef71358579d47fcd8f19040af33ba6d99a2eede
Louise <lowa1661@gmail.com>
2022-08-03 23:23:05 +0200

    Formatting

--- food-backend/src/services/ingredient_service.py
+++ food-backend/src/services/ingredient_service.py
@@ -1,13 +1,14 @@
-from sqlalchemy.exc import IntegrityError, NoResultFound
 from flask import Blueprint, jsonify, request, make_response
 
 from src.entities.entity import Session
 from src.entities.ingredient import Ingredient, IngredientSchema
+from src.services.ingredient_utils import handle_ingredient_crud, check_rating_range
 
 ingredient_blueprint = Blueprint("ingredient_blueprint", __name__)
 
 
 @ingredient_blueprint.route("/ingredients")
+@handle_ingredient_crud
 def get_ingredients():
     # Fetching ingredients from the database
     session = Session()
@@ -23,88 +24,63 @@ def get_ingredients():
 
 
 @ingredient_blueprint.route("/ingredients", methods=["POST"])
+@handle_ingredient_crud
 def add_ingredient():
-    try:
-        posted_ingredient = IngredientSchema(only=("name", "rating")).load(
-            request.get_json()
-        )
+    posted_ingredient = IngredientSchema(only=("name", "rating")).load(
+        request.get_json()
+    )
 
-        # Check rating range
-        if 10 < posted_ingredient["rating"] or posted_ingredient["rating"] < 0:
-            raise ValueError
+    check_rating_range(posted_ingredient["rating"])
 
-        session = Session()
-        # Check if name already exists
-        if (
-            session.query(Ingredient)
-            .filter(Ingredient.name == posted_ingredient["name"])
-            .first()
-            is not None
-        ):
-            raise NameError
+    session = Session()
+    # Check if name already exists
+    if (
+        session.query(Ingredient)
+        .filter(Ingredient.name == posted_ingredient["name"])
+        .first()
+        is not None
+    ):
+        raise NameError
 
-        ingredient = Ingredient(**posted_ingredient)
-        session.add(ingredient)
-        session.commit()
+    ingredient = Ingredient(**posted_ingredient)
+    session.add(ingredient)
+    session.commit()
 
-        # Return created ingredient
-        new_ingredient = IngredientSchema().dump(ingredient)
-        session.close()
-        return jsonify(new_ingredient), 201
-    except ValueError:
-        error_message = "Rating must fall in range [0, 10]."
-    except (NameError, IntegrityError):
-        error_message = "Ingredient name already exists."
-    except Exception:
-        error_message = "Something went wrong."
-    return make_response(error_message, 400)
+    # Return created ingredient
+    new_ingredient = IngredientSchema().dump(ingredient)
+    session.close()
+    return jsonify(new_ingredient), 201
 
 
 @ingredient_blueprint.route("/ingredient/<int:ingredient_id>", methods=["PUT"])
+@handle_ingredient_crud
 def put_ingredient(ingredient_id):
-    try:
-        data = request.get_json()
+    data = request.get_json()
 
-        # Check rating range
-        if 10 < data["rating"] or data["rating"] < 0:
-            raise ValueError
+    check_rating_range(data["rating"])
 
-        session = Session()
-        ingredient_object = (
-            session.query(Ingredient).filter(Ingredient.id == ingredient_id).one()
-        )
-        ingredient_object.name = data["name"]
-        ingredient_object.rating = data["rating"]
-        session.commit()
+    session = Session()
+    ingredient_object = (
+        session.query(Ingredient).filter(Ingredient.id == ingredient_id).one()
+    )
+    ingredient_object.name = data["name"]
+    ingredient_object.rating = data["rating"]
+    session.commit()
 
-        # Return edited ingredient
-        ingredient = IngredientSchema().dump(ingredient_object)
-        session.close()
-        return jsonify(ingredient), 200
-    except ValueError:
-        error_message = "Rating must fall in range [0, 10]."
-    except IntegrityError:
-        error_message = "Ingredient name already exists."
-    except NoResultFound:
-        error_message = "Ingredient does not exist."
-    except Exception:
-        error_message = "Something went wrong."
-    return make_response(error_message, 400)
+    # Return edited ingredient
+    ingredient = IngredientSchema().dump(ingredient_object)
+    session.close()
+    return jsonify(ingredient), 200
 
 
 @ingredient_blueprint.route("/ingredient/<int:ingredient_id>", methods=["DELETE"])
+@handle_ingredient_crud
 def delete_ingredient(ingredient_id):
-    try:
-        session = Session()
-        ingredient_object = (
-            session.query(Ingredient).filter(Ingredient.id == ingredient_id).one()
-        )
-        session.delete(ingredient_object)
-        session.commit()
-        session.close()
-        return make_response("Ingredient has been deleted.", 204)
-    except NoResultFound:
-        error_message = "Ingredient does not exist."
-    except Exception:
-        error_message = "Something went wrong."
-    return make_response(error_message, 400)
+    session = Session()
+    ingredient_object = (
+        session.query(Ingredient).filter(Ingredient.id == ingredient_id).one()
+    )
+    session.delete(ingredient_object)
+    session.commit()
+    session.close()
+    return make_response("Ingredient has been deleted.", 200)
---
+++ food-backend/src/services/ingredient_utils.py
@@ -0,0 +1,27 @@
+from functools import wraps
+
+from flask import make_response
+from sqlalchemy.exc import IntegrityError, NoResultFound
+
+
+def check_rating_range(rating):
+    if 10 < rating or rating < 0:
+        raise ValueError
+
+
+def handle_ingredient_crud(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        try:
+            return f(*args, **kwargs)
+        except ValueError:
+            error_message = "Rating must fall in range [0, 10]."
+        except NoResultFound:
+            error_message = "Ingredient does not exist."
+        except (NameError, IntegrityError):
+            error_message = "Ingredient name already exists."
+        except Exception:
+            error_message = "Something went wrong."
+        return make_response(error_message, 400)
+
+    return decorated
